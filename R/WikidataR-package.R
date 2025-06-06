f2bb4077c7aeadcc0ee3ffc4ec8d1d58af2bdb78
olivroy <olivierroy71@hotmail.com>
2025-06-06 10:15:46 -0400

    clean-up deprecation warning

--- R/WikidataR-package.R
+++ R/WikidataR-package.R
@@ -1,14 +1,14 @@
-#' @title API client library for Wikidata
 #' @description This package serves as an API client for reading and writing
 #' to and from \href{https://www.wikidata.org/wiki/Wikidata:Main_Page}{Wikidata}, (including 
 #' via the \href{https://quickstatements.toolforge.org/}{QuickStatements} format),
 #' as well as for reading from \href{https://www.wikipedia.org}{Wikipedia}.
-#' @name WikidataR
-#' @docType package
 #' @seealso \code{\link{get_random}} for selecting a random item or property,
 #' \code{\link{get_item}} for a /specific/ item or property, or \code{\link{find_item}}
 #' for using search functionality to pull out item or property IDs where the descriptions
 #' or aliases match a particular search term.
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import WikidataQueryServiceR
 #' @import tibble
 #' @import dplyr
@@ -25,5 +25,5 @@
 #' @importFrom WikipediR page_content random_page query
 #' @importFrom httr user_agent
 #' @importFrom jsonlite fromJSON
-#' @aliases WikidataR WikidataR-package
+## usethis namespace: end
 NULL
